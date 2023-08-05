5483ca4e6e065a52296e6af0c6618d1bd401fc9d
Kasumi Okuda <ec2-user@ip-172-31-28-235.ap-northeast-1.compute.internal>
2023-08-05 05:58:59 +0000

    [Fix]typo in item migration file

--- db/migrate/20230804221920_create_items.rb
+++ db/migrate/20230804221920_create_items.rb
@@ -5,7 +5,7 @@ class CreateItems < ActiveRecord::Migration[6.1]
       t.text :introduction, null: false
       t.integer :price, null: false
 
-      # t.datetime :created_at, null: false
+      #t.datetime :created_at, null: false
       #t.datetime :updated_at, null: false
 
       t.timestamps null: false
