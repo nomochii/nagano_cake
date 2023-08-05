34f7d1c281f7b2108e6ef939c6d35be2dd46fb13
Kasumi Okuda <ec2-user@ip-172-31-28-235.ap-northeast-1.compute.internal>
2023-08-05 05:18:24 +0000

    [Fix]typo in admin model

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,37 @@
 Rails.application.routes.draw do
-  devise_for :customers
+  devise_for :customers, controllers: {
+  sessions:      'users/sessions',
+  passwords:     'users/passwords',
+  registrations: 'users/registrations'
+  }
+  devise_for :admins, controllers: {
+  sessions:      'admins/sessions',
+  passwords:     'admins/passwords',
+  registrations: 'admins/registrations'
+  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'top' => 'homes#top'
   get 'about' => 'homes#about'
+
+  namespace :admin do
+    resources :admins
+    resources :customers
+    resources :items
+    resources :order_details_
+    resources :orders
+    resources :homes
+  end
+
+  namespace :public do
+    resources :admins
+    resources :customers
+    resources :items
+    resources :order_details_
+    resources :orders
+    resources :cart_items
+    resources :homes
+  end
+
 
 
 end
